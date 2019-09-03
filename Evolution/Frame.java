696c4c7cfaf30136c349791db8a5f7960faefc3a
Sky Vercauteren <31872243+yoayo112@users.noreply.github.com>
2019-09-02 19:40:32 -0600

    Add files via upload

--- Evolution/Frame.java
+++ Evolution/Frame.java
@@ -15,6 +15,10 @@ public class Frame extends JFrame implements MouseListener, MouseMotionListener 
     public boolean l;
     public boolean g;
     public boolean dragging;
+    public int boardX;
+    public int boardY;
+    public int pixelSize;
+    
     public Frame(String title)
     {
         super(title);
@@ -40,21 +44,37 @@ public class Frame extends JFrame implements MouseListener, MouseMotionListener 
         Point point = event.getPoint();
         p1 = point;
         dragging = true;
-        if(p == true)
-        {
-            sim.addBarrier(p1);
-            p = false;
-        }
     }
  
     @Override
     public void mouseReleased(MouseEvent event) {
         Point point = event.getPoint();
         p2 = point;
+        boardX = sim.board.getX();
+        boardY= sim.board.getY();
+        boardX += this.getContentPane().getX();
+        boardY += this.getContentPane().getY();
+        p1.x -= boardX;
+        p1.y -= boardY;
+        p2.x -= boardX;
+        p2.y -= boardY;
+        int x1 = (int) p1.x/pixelSize;
+        int y1 = (int) p1.y/pixelSize;
+        int x2 = (int) p2.x/pixelSize;
+        int y2 = (int) p2.y/pixelSize;
+        p1.x = x1;
+        p1.y = y1;
+        p2.x = x2;
+        p2.y = y2;
         dragging = false;
-        if(l == true)
+        if(p == true)
         {
-            sim.addBarrierLine(p1.x,p1.y,p2.x,p2.y);
+            sim.addBarrier(p1);
+            p = false;
+        }
+        else if(l == true)
+        {
+            sim.addBarrierLine(x1,y1,x2,y2);
             l = false;
         }
         else if(g == true)
