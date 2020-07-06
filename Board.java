af04a2f9e5dbbb44afe40cdd787813537a247a16
Sean <Sean@DESKTOP-C97ETRN>
2020-07-05 19:35:36 -0600

    Updated attacking. Fixed 0 food value bug

--- Board.java
+++ Board.java
@@ -8,61 +8,52 @@ import javax.swing.JPanel;
 
 public class Board extends JPanel {
 
-    private BufferedImage canvas;
-    private int pixelSize; 
-    private int boardSize;
+	private BufferedImage canvas;
+	private int pixelSize;
+	private int boardSize;
 
-    public Board(int pixelSize, int bs) {
-        this.pixelSize = pixelSize; 
-        boardSize = bs;
-        canvas = new BufferedImage(boardSize * pixelSize, boardSize * pixelSize, BufferedImage.TYPE_INT_ARGB);
-        fillCanvas(Color.black);
-    }
-    
-    public void setBoardSize(int s)
-    {
-        boardSize = s;
-    }
-    
-    public Dimension getPreferredSize() {
-        return new Dimension(canvas.getWidth(), canvas.getHeight());
-    }
+	public Board(int pixelSize, int bs) {
+		this.pixelSize = pixelSize;
+		boardSize = bs;
+		canvas = new BufferedImage(boardSize * pixelSize, boardSize * pixelSize, BufferedImage.TYPE_INT_ARGB);
+		fillCanvas(Color.black);
+	}
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-        g2.drawImage(canvas, null, null);
-    }
+	public void setBoardSize(int s) {
+		boardSize = s;
+	}
 
+	public Dimension getPreferredSize() {
+		return new Dimension(canvas.getWidth(), canvas.getHeight());
+	}
 
-    public void fillCanvas(Color c) {
-        int color = c.getRGB();
-        for (int x = 0; x < canvas.getWidth(); x++)
-        {
-            for (int y = 0; y < canvas.getHeight(); y++)
-            {
-                canvas.setRGB(x, y, color);
-            }
-        }
-    }
-    
-    public void draw(Point point, Color c)
-    {
-        int color = c.getRGB();
-        int x1 = point.x;
-        int y1 = point.y;
-        // Implement rectangle drawing
-        for (int x = x1 * pixelSize; x < pixelSize * (x1 + 1); x++)
-        {
-            for (int y = y1 * pixelSize; y < pixelSize * (y1 + 1); y++)
-            {
-                canvas.setRGB(x, y, color);
-            }
-        }
-    }
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(canvas, null, null);
+	}
 
-    public void erase(Point point)
-    {
-        draw(point, Color.black);
-    }
+	public void fillCanvas(Color c) {
+		int color = c.getRGB();
+		for (int x = 0; x < canvas.getWidth(); x++) {
+			for (int y = 0; y < canvas.getHeight(); y++) {
+				canvas.setRGB(x, y, color);
+			}
+		}
+	}
+
+	public void draw(Point point, Color c) {
+		int color = c.getRGB();
+		int x1 = point.x;
+		int y1 = point.y;
+		for (int x = x1 * pixelSize; x < pixelSize * (x1 + 1); x++) {
+			for (int y = y1 * pixelSize; y < pixelSize * (y1 + 1); y++) {
+				canvas.setRGB(x, y, color);
+			}
+		}
+	}
+
+	public void erase(Point point) {
+		draw(point, Color.black);
+	}
 }
--- Frame.java
+++ Frame.java
@@ -7,126 +7,116 @@ import java.awt.event.MouseMotionListener;
 import javax.swing.JFrame;
 
 public class Frame extends JFrame implements MouseListener, MouseMotionListener {
-    public Simulator sim;
-    public int bs;
-    public Point p1;
-    public Point p2;
-    public boolean p;
-    public boolean l;
-    public boolean g;
-    public boolean e;
-    public boolean dragging;
-    public int boardX;
-    public int boardY;
-    public int pixelSize;
-    public int xSpace;
-    public int xOff;
-    public int ySpace;
-    public int yOff;
-    
-    
-    public Frame(String title)
-    {
-        super(title);
-        addMouseListener(this);
-        addMouseMotionListener(this);
-        dragging = false;
-    }
-    @Override
-        public void mouseClicked(MouseEvent event) {
-        
-    }
- 
-    @Override
-    public void mouseEntered(MouseEvent event) {
-    }
- 
-    @Override
-    public void mouseExited(MouseEvent event) {
-    }
-    
-   @Override
-    public void mousePressed(MouseEvent event) {
-        Point point = event.getPoint();
-        p1 = point;
-        dragging = true;
-    }
- 
-    @Override
-    public void mouseReleased(MouseEvent event) {
-        Point point = event.getPoint();
-        p2 = point;
-        boardX = sim.board.getX();
-        boardY = sim.board.getY();
-        boardX += this.getContentPane().getX();
-        boardY += this.getContentPane().getY();
-        p1.x -= boardX + 8;
-        p1.y -= boardY + 32;
-        p2.x -= boardX + 8;
-        p2.y -= boardY + 32;
-        int x1 = p1.x/pixelSize;
-        int y1 = p1.y/pixelSize;
-        int x2 = p2.x/pixelSize;
-        int y2 = p2.y/pixelSize;
-        p1.x = x1;
-        p1.y = y1;
-        p2.x = x2;
-        p2.y = y2;
-        dragging = false;
-        if(p == true)
-        {
-            sim.addBarrier(p1);
-        }
-        else if(l == true)
-        {
-            sim.addBarrierLine(x1,y1,x2,y2);
-        }
-        else if(g == true)
-        {
-            sim.addBarrierGraph(x1,y1,x2,y2,xSpace,ySpace,xOff,yOff);
-        }
-        else if (e == true)
-        {
-            sim.removeBarrierRect(x1,y1,x2,y2);
-        }
-    }
-    
-    @Override
-    public void mouseDragged(MouseEvent event) {
-    }
-    
-     @Override
-    public void mouseMoved(MouseEvent e) {
-    }
-    
-    public void lTrue()
-    {
-        l = true;
-        p = false;
-        g = false;
-        e = false;
-    }
-    public void pTrue()
-    {
-        p = true;
-        l = false;
-        g = false;
-        e = false;
-    }
-    public void gTrue()
-    {
-        g = true;
-        p = false;
-        l = false;
-        e = false;
-    }
-    public void eTrue()
-    {
-        e = true;
-        p = false;
-        g = false;
-        l = false;
-    }
+	public Simulator sim;
+	public int bs;
+	public Point p1;
+	public Point p2;
+	public boolean p;
+	public boolean l;
+	public boolean g;
+	public boolean e;
+	public boolean dragging;
+	public int boardX;
+	public int boardY;
+	public int pixelSize;
+	public int xSpace;
+	public int xOff;
+	public int ySpace;
+	public int yOff;
 
-   
+	public Frame(String title) {
+		super(title);
+		addMouseListener(this);
+		addMouseMotionListener(this);
+		dragging = false;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent event) {
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent event) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent event) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent event) {
+		Point point = event.getPoint();
+		p1 = point;
+		dragging = true;
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent event) {
+		Point point = event.getPoint();
+		p2 = point;
+		boardX = sim.board.getX();
+		boardY = sim.board.getY();
+		boardX += this.getContentPane().getX();
+		boardY += this.getContentPane().getY();
+		p1.x -= boardX + 8;
+		p1.y -= boardY + 32;
+		p2.x -= boardX + 8;
+		p2.y -= boardY + 32;
+		int x1 = p1.x / pixelSize;
+		int y1 = p1.y / pixelSize;
+		int x2 = p2.x / pixelSize;
+		int y2 = p2.y / pixelSize;
+		p1.x = x1;
+		p1.y = y1;
+		p2.x = x2;
+		p2.y = y2;
+		dragging = false;
+		if (p == true) {
+			sim.addBarrier(p1);
+		} else if (l == true) {
+			sim.addBarrierLine(x1, y1, x2, y2);
+		} else if (g == true) {
+			sim.addBarrierGraph(x1, y1, x2, y2, xSpace, ySpace, xOff, yOff);
+		} else if (e == true) {
+			sim.removeBarrierRect(x1, y1, x2, y2);
+		}
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent event) {
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+	}
+
+	public void lTrue() {
+		l = true;
+		p = false;
+		g = false;
+		e = false;
+	}
+
+	public void pTrue() {
+		p = true;
+		l = false;
+		g = false;
+		e = false;
+	}
+
+	public void gTrue() {
+		g = true;
+		p = false;
+		l = false;
+		e = false;
+	}
+
+	public void eTrue() {
+		e = true;
+		p = false;
+		g = false;
+		l = false;
+	}
+
 }
